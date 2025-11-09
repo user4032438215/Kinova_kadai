ab05918f92351ba09d9e682bc2d23ad23af43095
user4032438215 <sigonghuiren@gmail.com>
2025-11-09 11:34:42 +0900

    BD一覧にkinova_sampleというDBはあるが、テーブルが無い状態、テーブルを作成する必要があり

--- kinova-webapp-tasks/kinova_webapp_tasks_5/kinova-webapp-task-5-crud/index.js
+++ kinova-webapp-tasks/kinova_webapp_tasks_5/kinova-webapp-task-5-crud/index.js
@@ -4,7 +4,7 @@ const { Pool } = require("pg"); // PostgreSQL用ライブラリを読み込む
 const pool = new Pool({
   user: "postgres",       
   host: "localhost",      
-  database: "postkinova_samplegres",   // ※データベース講座で作成した DB 名
+  database: "kinova_sample",   // ※データベース講座で作成した DB 名
   password: "19990120", 
   port: 5432              
 });
